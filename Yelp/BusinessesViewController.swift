03baed21afbd5d3f342636150ddf71e89c2b9e29
dreamsicl <vphong@ucsd.edu>
2017-02-21 23:24:55 -0800

    search bar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     var businesses: [Business]!
     
+    var filteredBusinesses: [Business]!
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    lazy var searchBar = UISearchBar();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
         
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.filteredBusinesses = self.businesses
             self.tableView.reloadData()
             
             if let businesses = businesses {
@@ -69,6 +79,23 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         
         return cell
         
+    }
+    
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let businesses = self.businesses
+            else {
+                return
+        }
+        
+        filteredBusinesses = searchText.isEmpty ? businesses : businesses.filter({business in
+            
+            let foundBusiness = (business.name)!.range(of: searchText, options: .caseInsensitive) != nil
+            return foundBusiness
+            
+        })
+        
+        tableView.reloadData()
     }
     
     /*
